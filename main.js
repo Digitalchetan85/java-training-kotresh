2cbba664408eaca07ab7bc43cb96ab0e643cef81
Chetan Kumar N <chetankumar.nv@gmail.com>
2021-09-22 14:28:55 +0530

    Java training 1-13 videos

---
+++ main.js
@@ -0,0 +1,177 @@
+//console.log("Wel cone to Java script");
+
+//alert("welcome");
+
+//document.write("welcome");
+
+// var da = document.getElementById("data");
+
+// console.log(da);
+// da.innerHTML = "welcome";
+
+// da.style.color = "red";
+
+// var a = "chetan", b = 5;
+
+// //var b = 5;
+
+// var c = a + b;
+
+// console.log(c);
+
+// console.log(a + b);
+
+// var x = 20, y = 20;
+
+// x += y;
+
+// x = x + y;
+
+// var x = 20, y = 20;
+
+// var z = x + y;
+
+// console.log(z)
+// console.log(typeof z)
+
+// console.log(typeof y)
+
+    
+    
+// if (z === "40")
+// {
+//     console.log("no");
+// }
+// else 
+// {
+//     console.log(z);
+// }
+
+// var a = 2, b = 3;
+
+// if (a === "2")
+// {
+//     document.write("hello");
+// }
+// else 
+// {
+//     document.write('sorry');
+// }
+
+// var a = 5;
+
+// switch(a)
+// {
+//     case 0: 
+//         document.write("Order Placed");
+//         break;
+
+//     case 1:
+//         document.write("Order Processing");
+//         break;
+
+//     case 2:
+//         document.write("Order Processed");
+//         break;
+
+//     case 3:
+//         document.write("Payment needs to be done");
+//         break;
+
+//     default: 
+//         document.write("contact Admin");
+// }
+
+
+
+/*for (var i = 2; i <= 2; i++)
+{
+    for(var j = 10; j >= 1; j--)
+    {
+        a = "";
+       // document.write(i+"*"+j +"=" +i*j + "<br>");
+       a = document.getElementById("data");
+
+       a.innerHTML += ("<br>" +i+"*"+j +"=" +i*j );
+    }
+}*/
+
+
+// for (var i = 2; i <= 30; i++)
+// {
+//     for (var j = 2; j < i; j++)
+//     {
+//         if (i % j == 0) 
+//         {
+//             break;
+//         } 
+//     }
+
+//     if (i == j) {
+//         //document.write("Prime No = "+i+"<br>");
+//         var a = document.getElementById("data");
+
+//         a.innerHTML +=  ("Prime No = "+i+"<br>");
+//     }
+// }
+
+// var i = 1;
+
+// var table = 3776;
+
+// while (i <= 10) {
+//     //document.write("Prime No = "+i+"<br>");
+//     var a = document.getElementById("data");
+
+//     a.innerHTML +=  (table+"*"+i+"="+table*i+"<br>");
+
+//     i++;
+// }
+
+// var i = 1;
+
+// var table = 6;
+
+// do {
+//     //document.write("Prime No = "+i+"<br>");
+//     var a = document.getElementById("data");
+
+//     a.innerHTML +=  (table+"*"+i+"="+table*i+"<br>");
+
+//     i++;
+// } while (i <= 10)
+
+
+// function a() {
+//     alert("hello");
+// }
+
+// a();
+
+// var a = new function() {
+//     alert("second");
+// }
+
+// a();
+
+// hen = () => {
+//     alert("three");
+// }
+
+// hen();
+
+// var x,y,z;
+
+// function add(x,y) {
+//     z = x + y;
+    
+//     //document.write(z);
+//     return z;
+// }
+
+// add(10,20);
+
+// a = document.getElementById("data");
+
+// a.innerHTML = z;
+
